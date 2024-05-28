fe341d315ceaba0d9dd742fd0d7bb527d22f3ef8
Binshabs <binshabs567@gmail.com>
2024-05-28 16:12:39 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule],
+})
+export class AppComponent {
+  componentTitle = "My To Do List";
+
+  //filter: "all" | "active" | "done" = "all";
+filler:"all" | " active"| "done" = "all";
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filler === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filler === "done" ? item.done : !item.done
+    );
+  }
+  addItem(description: string) {
+  if (!description) return;
+
+  this.allItems.unshift({
+    description,
+    done: false
+  });
+  
+
+}
+
+}
